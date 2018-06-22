346943951a2d949499a6af7d9fcd546ae57460ff
Eugen Severins <e.severins@everest.nl>
2018-06-22 12:40:01 +0200

    Tweede dataset

--- JiraFunctions.py
+++ JiraFunctions.py
@@ -11,7 +11,7 @@ def GetStory (issuenr):
 
 def GetStories (Year, maxResults):
     jc = connect()
-    issues_in_proj = jc.search_issues('project=EMSPM AND createdDate > '+str(Year)+'  AND type =  Story ORDER BY createdDate DESC', expand='changelog',maxResults=maxResults)
+    issues_in_proj = jc.search_issues('project=EMSPM AND createdDate > "'+str(Year)+'-01-01"  AND type =  Story AND assignee != null ORDER BY createdDate DESC  ', expand='changelog',maxResults=maxResults)
     return issues_in_proj
 
 def GetTransitions (stories):
@@ -23,6 +23,25 @@ def GetTransitions (stories):
                     # TODO: Niet printen maar retourneren als tuple
                     print(issue.key + " Date: "+ history.created + " " + item.fromString+" --> "+ item.toString)
 
+def StripSummary(text):
+    text = text.replace("CLONE - ","")
+    if text.lower().find("bouw") != -1 or text.find("BE") != -1 or  text.find("TE") != -1:
+        text = "Bouw"
+
+    if text.lower().find("done") != -1 or text.lower().find("dod") != -1 :
+        text = "Definition of done"
+
+    if text.lower().find("uitvoer") != -1 or text.lower().find("test") != -1 :
+        text = "Uitvoeren systeemtest"
+
+    if text.lower().find("voorbereid") != -1 or text.lower().find("voor") != -1 :
+        text = "Voorbereiden systeemtest"
+
+    if text.lower().find("pm") != -1 :
+        text = "PM-aanpassing"
+
+    return text
+
 def GetTransitionsList (stories):
     row = list()
     for issue in stories:
@@ -31,7 +50,7 @@ def GetTransitionsList (stories):
             for item in history.items:
                 if item.field == 'status':
                     # TODO: Niet printen maar retourneren als tuple
-                    row.append((issue.key, history.created, item.fromString))
+                    row.append((issue.key, history.created, StripSummary(issue.fields.summary), item.fromString))
     return row
 def GetSubtasks (stories):
     jc = connect()
--- main.py
+++ main.py
@@ -6,18 +6,18 @@ from GenerateData import *
 # create logger
 logging.basicConfig(filename='example.log',level=logging.INFO)
 
-WriteHeader(("Key","EndTimestamp","Status"))
+WriteHeader(("Key","EndTimestamp","Summary","Status"))
 #actual code
 #key, summary, description = GetIssueFields("EMSPM-14628")
-def EnkeleStory():
-    story = GetStory("EMSPM-14847")
+def EnkeleStory(story):
+    story = GetStory(story)
     subtasks = GetSubtasks(story)
     result = GetTransitionsList(subtasks)
     for row in result:
         AppendRow(row)
 
 def AlleStories():
-    stories = GetStories(2018, maxResults="50")
-    Key, EndTimestamp, Status =  GetTransitions(stories)
-
-EnkeleStory()
+    stories = GetStories(2018, maxResults="500")
+    for story in stories:
+        EnkeleStory(story.key)
+AlleStories()
